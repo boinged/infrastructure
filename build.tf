05585c02eb030dcbf917b2a4d1274dd3ff6848d9
boinged <stevenshipton@hotmail.com>
2020-04-13 14:57:24 +0100

    add cloud build for env

--- build.tf
+++ build.tf
@@ -1,6 +1,6 @@
 resource "google_cloudbuild_trigger" "ci_trigger" {
   provider = google-beta
-  name     = "website-ci"
+  name     = "web-ci"
 
   github {
     owner = "boinged"
@@ -9,6 +9,27 @@ resource "google_cloudbuild_trigger" "ci_trigger" {
     push {
       branch = "master"
     }
+  }
+
+  filename = "cloudbuild.yaml"
+}
+
+resource "google_cloudbuild_trigger" "cd_trigger" {
+  provider = google-beta
+  name     = "web-cd"
+
+  github {
+    owner = "boinged"
+    name  = "web-env"
+
+    push {
+      branch = "master"
+    }
+  }
+
+  substitutions = {
+    _CLUSTER  = var.cluster
+    _LOCATION = var.location
   }
 
   filename = "cloudbuild.yaml"
