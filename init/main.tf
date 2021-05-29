52603372798f6624f5344adeafd1d2eba3c2ae34
boinged <stevenshipton@hotmail.com>
2021-05-29 17:05:22 +0000

    Configure tfstate bucket

--- init/main.tf
+++ init/main.tf
@@ -4,4 +4,20 @@ data "google_client_config" "current" {
 resource "google_storage_bucket" "state_bucket" {
   name     = "${data.google_client_config.current.project}-tfstate"
   location = "EUROPE-WEST2"
+
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    condition {
+      num_newer_versions = 3
+    }
+
+    action {
+      type = "Delete"
+    }
+  }
 }
